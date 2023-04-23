8679c79b720be1a76ad501117c14b26378cc0f0c
ichigo-dev <dev.honda.ichigo@gmail.com>
2023-04-23 13:36:10 +0900

    add: Add solution for task 001

---
+++ rust/task_001/src/main.rs
@@ -0,0 +1,20 @@
+/*
+
+    # 001 - Print 5+N
+
+    https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_a
+
+*/
+
+use algorithm_util::*;
+
+fn main()
+{
+    let apple_num: usize = 5;
+    let orange_num: usize = input();
+
+    assert!(1<=orange_num);
+    assert!(orange_num<=100);
+
+    println!("{}", apple_num + orange_num);
+}
