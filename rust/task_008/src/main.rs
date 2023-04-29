7aacc7048dabadfa196a021dcd4936799b3ac052
ichigo-dev <dev.honda.ichigo@gmail.com>
2023-04-29 09:48:02 +0900

    add: Add solution for task 008

---
+++ rust/task_008/src/main.rs
@@ -0,0 +1,32 @@
+/*
+
+    008 - Brute Force 1
+
+    https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_h
+
+*/
+
+use algorithm_util::*;
+
+fn main()
+{
+    let inputs: Vec<usize> = input_vec();
+    assert!(inputs.len() == 2);
+
+    let (n, s) = (inputs[0], inputs[1]);
+    assert!(n >= 1 && n <= 1000);
+    assert!(s >= 1 && s <= 2000);
+
+    let mut cnt = 0;
+    for i in 1..(n+1)
+    {
+        for j in 1..(n+1)
+        {
+            if i + j <= s
+            {
+                cnt += 1;
+            }
+        }
+    }
+    println!("{}", cnt);
+}
