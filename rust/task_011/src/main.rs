620ced5906143af03188dd5a5bc7ddfdcffcb930
ichigo-dev <dev.honda.ichigo@gmail.com>
2023-04-29 12:51:13 +0900

    add: Add solution for task 011

---
+++ rust/task_011/src/main.rs
@@ -0,0 +1,68 @@
+/*
+
+    011 - Print Prime Numbers
+
+    https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_k
+
+*/
+
+#![allow(dead_code)]
+use algorithm_util::*;
+
+fn is_prime( n: usize ) -> bool
+{
+    assert!(n > 1);
+    for i in 2..n
+    {
+        if n % i == 0
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn main()
+{
+    let n: usize = input();
+    assert!(n >= 2 && n <= 3000);
+
+    //  全探索
+    //
+    //  let mut prime_numbers = Vec::new();
+    //  for i in 2..(n+1)
+    //  {
+    //      if is_prime(i)
+    //      {
+    //          prime_numbers.push(i.to_string());
+    //      }
+    //  }
+    //  println!("{}", prime_numbers.join(" "));
+
+    //  エラトステネスの篩
+    let mut prime_flags = vec![true; n];
+    prime_flags[0] = false;
+    prime_flags[1] = false;
+    for i in 0..n
+    {
+        if prime_flags[i] == false
+        {
+            continue;
+        }
+
+        for j in (i*i..n).step_by(i)
+        {
+            prime_flags[j] = false;
+        }
+    }
+
+    let mut prime_numbers = Vec::new();
+    for (i, v) in prime_flags.iter().enumerate()
+    {
+        if *v
+        {
+            prime_numbers.push(i.to_string());
+        }
+    }
+    println!("{}", prime_numbers.join(" "));
+}
